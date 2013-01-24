1e3ecac271b747ef0a7678661563d52c8c2111cc
Richard Kelsey <r.i.kelsey@gmail.com>
2013-01-24 22:15:41 +0000

    bitstring doesn't work on large files
    refactoring with a generator

--- mpeg_fix_si.py
+++ mpeg_fix_si.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
+import time
 import sys
 from bitstring import BitArray, BitStream, ConstBitStream
 
+#class gen:
+#	def __init__(self, fn):
+#		self.f = fn
+#	def next(self):
+#		return self.f()
+#	def __iter__(self):
+#		return self
 
 class TsPacketFileIO:
 
@@ -13,13 +21,40 @@ class TsPacketFileIO:
 
 	wr = 'read'
 
+#	def open(self, filename, wr='read'):
+#		self.wr = wr
+#		self.filename = filename
+#		try:
+#			if wr == 'read':
+#				print 'read ' + self.filename
+#				self.filestream = ConstBitStream( filename=self.filename )
+#				self.byte_reader = self.filestream.cut(188*8)
+#			else:
+#				self.filestream = open(filename, 'wb')
+#		except Exception, e:
+#			raise e
+
 	def open(self, filename, wr='read'):
 		self.wr = wr
 		self.filename = filename
 		try:
 			if wr == 'read':
-				self.filestream = ConstBitStream( filename=self.filename )
-				self.byte_reader = self.filestream.cut(188*8)
+				print 'read ' + self.filename
+				self.filestream = open( self.filename, 'rb' )
+				
+				class gen:
+					def __init__ (self, s):
+						self.s = s
+					def next(self):
+						temp = self.s.read(188)
+						if len(temp) != 188:
+							raise StopIteration
+						else:
+							return bytearray(temp)
+					def __iter__(self):
+						return self
+					
+				self.byte_reader = gen(self.filestream)		
 			else:
 				self.filestream = open(filename, 'wb')
 		except Exception, e:
@@ -40,36 +75,60 @@ class TsPacketFileIO:
 
 if __name__ == '__main__':
 
+	os_starttime = time.clock()
+	starttime = time.time()
+	
 	input_filename = sys.argv[1]
 
 	reader = TsPacketFileIO()
 	reader.open(filename=input_filename)
+	
+#	newPAT = TsPacketFileIO()
+#	newPAT.open('pat.ts', 'read')
+#	nPAT = next(newPAT.byte_reader)
+#
+#	newPMT = TsPacketFileIO
+#	newPMT.open('pmt.ts', 'read')
+#	nPMT = next(newPAT.byte_reader)
+#	
+#	newSDT = TsPacketFileIO()
+#	newSDT.open('sdt.ts', 'read')
+#	nSDT = next(newSDT.byte_reader)
+	
+#	newStream = TsPacketFileIO()
+#	newStream.open(filename=sys.argv[2], wr='write')
 
-	PATwriter = TsPacketFileIO()
-	PATwriter.open(filename=pat.ts, wr='write')
-
-	PMTwriter = TsPacketFileIO()
-	PMTwriter.open(filename=pmt.ts, wr='write')
-
-	SDTwriter = TsPacketFileIO()
-	SDTwriter.open(filename=sdt.ts, wr='write')
-
+	pids = {}
+		
 	for packet in reader.byte_reader:
 
-		PID = packet[11:24]
+		PID = (BitArray(packet))[11:24]
 
-		if '0x1' == PID.uint:
+#		if PID.uint == 0:
+#			newStream.writepacket(nPAT)
+#		elif PID.uint == 17:
+#			newStream.writepacket(nSDT)
+#		elif PID.uint == 32:
+#			newStream.writepacket(nPMT)
+#		else:
+#			newStream.writepacket(packet)
+		
 
-			PATwriter.writepacket(packet)
-    
-	elif '0x20' == PID.uint:
-        
-		PMTwrite.writepacket(packet)
-        
-	elif '0x11' == PID.uint:
-        
-		SDTwriter.writepacket(packet)
-        
+		p = str(PID.uint)
+				
+		if p in pids:
+			pids[p] = pids[p] + 1
+		else:
+			pids[p] = 0
+			
+
+	print "PID\t\t:Number"
+	
+	for p in pids:
+		print p +"\t\t" + str(pids[p])
 
 	print "Done!"
+
+	print "CPU time: " + str( time.clock() - os_starttime)
+	print "TIme    : " + str( time.time() - starttime)
 
